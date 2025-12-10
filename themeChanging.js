f1c39a94db9ade341d2cd043d6eaff377b7b6901
Zahra Hassani <hassanizahra897@gmail.com>
2025-12-11 00:50:05 +0430

    Create themeChanging.js

---
+++ themeChanging.js
@@ -0,0 +1,27 @@
+const body = document.body;
+const h1 = document.getElementById("h1");
+const btn = document.getElementById("btn");
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+function green(){
+    body.style.backgroundColor = "darkgreen";
+};
+function purple(){
+    body.style.backgroundColor = "purple";
+};
+function blue(){
+    body.style.backgroundColor = "darkblue";
+};
+function red(){
+    body.style.backgroundColor = "darkred";
+};
+const colors =[
+"purple",
+"darkred",
+"black",
+"darkblue"
+];
+let index =0;
+
+
